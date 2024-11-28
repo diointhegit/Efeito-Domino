b03055d1d6ea6ed86f339eb33d48577e0646d9c7
Denis Silva <denisls02@outlook.com>
2024-11-28 17:12:41 -0300

    1.0 stable

--- src/app/app/(auth)/login/page.tsx
+++ src/app/app/(auth)/login/page.tsx
@@ -6,39 +6,6 @@ import { redirect } from "next/navigation";
 import { LoginForm } from "./loginform";
 
 export default async function Page() {
-  async function handleLogin(fd: FormData) {
-    "use server";
-
-    // const { data, error } = await supabase.auth.signInWithPassword({
-    //   email: fd.get("email") as string,
-    //   password: fd.get("password") as string,
-    // });
-    // if (error) {
-    //   redirect("/error");
-    // }
-    // console.log(data);
-    // revalidatePath("/", "layout");
-    // redirect("/login");
-
-    const supabase = createClient();
-
-    // type-casting here for convenience
-    // in practice, you should validate your inputs
-    const data = {
-      email: fd.get("email") as string,
-      password: fd.get("password") as string,
-    };
-
-    const { error } = await supabase.auth.signInWithPassword(data);
-
-    if (error) {
-      redirect("/error");
-    }
-
-    revalidatePath("/", "layout");
-    redirect("/home");
-  }
-
   return (
     <div className="w-[350px] shadow-2xl bg-primary border-dark-bg border-2 py-10 rounded-md text-light-text">
       <h1 className="text-4xl text-center mb-5 text-light-text font-bold">
